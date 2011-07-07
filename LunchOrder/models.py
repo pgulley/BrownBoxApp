47df94f316aec7308d5fd2c692c95dd1cb7f3d6b
Matthew Gallagher <mattva01@gmail.com>
2011-07-07 10:05:06 -0400

    More model changes

--- LunchOrder/models.py
+++ LunchOrder/models.py
@@ -3,13 +3,16 @@ from django.db import models
 # Create your models here
 
 class User(models.Model):
-	#User Info- need to get OpenId thing in here...
-	Department = models.CharField(max_length = 20)
-        Room = models.CharField(max_length = 4) 
+    #User Info- need to get OpenId thing in here...
+    Department = models.CharField(max_length = 20)
+    Room = models.CharField(max_length = 4) 
 class Order(models.Model):
-	User = models.ForeignKey(User)
-	Meal = models.ForeignKey(Meal)
+    User = models.ForeignKey(User)
+    Meal = models.ForeignKey(Meal)
 
 class Meal(models.Model):
-	Name = models.CharField(max_length = 20)
+    ingredients = models.ManyToManyField(Ingredient)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length = 20)  
 	
