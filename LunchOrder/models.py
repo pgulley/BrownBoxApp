f16d752ef51e70a525f6ac302d1ab2351b700f4f
Matthew Gallagher <mattva01@gmail.com>
2011-07-07 10:06:12 -0400

    Changed models for pep-8 compliance

--- LunchOrder/models.py
+++ LunchOrder/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class User(models.Model):
     #User Info- need to get OpenId thing in here...
-    Department = models.CharField(max_length = 20)
-    Room = models.CharField(max_length = 4) 
+    department = models.CharField(max_length = 20)
+    room = models.CharField(max_length = 4) 
 class Order(models.Model):
-    User = models.ForeignKey(User)
-    Meal = models.ForeignKey(Meal)
+    user = models.ForeignKey(User)
+    meal = models.ForeignKey(Meal)
 
 class Meal(models.Model):
     ingredients = models.ManyToManyField(Ingredient)
